6edca03384f8ac6313af06d63bf733da3467c180
Leonardo <lcat140507@gmail.com>
2026-06-22 21:26:58 +0100

    inicializar base de dados  com tabela equipas

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,23 @@
-console.log("Servidor Mundial a funcionar!");
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+async function iniciarBaseDados() {
+    const db = await open({
+        filename:'./mundial.db',
+        driver: sqlite3.Database
+    });
+
+    await db.exec(`
+        CREATE TABLE IF NOT EXISTS equipas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL UNIQUE,
+            grupo TEXT NOT NULL,
+            bandeira TEXT NOT NULL,
+            likes INTEGER DEFAULT 0
+        );
+    `);
+
+    console.log("Base de dados pronta e tabela de equipas criada!");
+}
+
+iniciarBaseDados();
