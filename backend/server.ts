6c330a32879964ecfb4b17e270b1eb9322d0a9df
Leonardo <lcat140507@gmail.com>
2026-06-26 23:18:47 +0100

    ligar base de dados e criar rota /equipas

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,26 @@
 import express from "express";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
 
 const app = express();
 const port = 3000;
+
+async function ligarBD() {
+    return open({
+        filename: "./mundial.db",
+        driver: sqlite3.Database,
+    });
+}
+
+app.get ("/equipas", async (req, res) => {
+    try {
+        const db = await ligarBD();
+        const equipas = await db.all("SELECT * FROM equipas");
+        res.json(equipas);
+    } catch (error) {
+        res.status(500).json({ error: "Erro ao ligar à base de dados" });
+    }
+});
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
