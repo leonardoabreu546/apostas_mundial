7ac3a9034721524f6662aef497fefecc18de35ae
Leonardo <lcat140507@gmail.com>
2026-07-05 22:43:57 +0100

    renderizar registoutilizador no app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import RegistoUtilizador from "./components/registoUtilizador";
+
 function App() {
   return (
-    <div className="container mt-5">
-      <h1 className="text-primary">Mundial - Registo de Utilizador</h1>
+    <div className="container mt-5" style={{ maxWidth: '500px' }}>
+      <h1 className="text-center mb-4 text-primary fw-bold">Mundial 2026</h1>
+      <RegistoUtilizador />
     </div>
   );
 }
