987a253dd964777f20c7e1fcf2a76cb161c469de
Leonardo <lcat140507@gmail.com>
2026-07-05 22:16:43 +0100

    inicializar estrutura do frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">Mundial - Registo de Utilizador</h1>
+    </div>
+  );
+}
+
+export default App;
